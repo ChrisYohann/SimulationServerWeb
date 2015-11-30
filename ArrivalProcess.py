40958dd4a62c8028af53b929cbb53d7699ea9ea2
Yohann Hako <i.nhyne@gmail.com>
2015-11-30 16:01:21 +0100

    confidence interval

--- ArrivalProcess.py
+++ ArrivalProcess.py
@@ -4,8 +4,9 @@ import Type1
 import Type2
 import random as rd
 import numpy as np
-import matplotlib.pyplot as plt
 import Queue as Q
+import scipy as sp
+import scipy.stats
 
 
 
@@ -37,6 +38,12 @@ class ArrivalProcess :
             i+=1
         return connected_users
 
+    def mean_confidence_interval(data, confidence=0.95):
+        a = 1.0*np.array(data)
+        n = len(a)
+        m, se = np.mean(a), scipy.stats.sem(a)
+        h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+        return m, m-h, m+h
 
 
 
